e8e0ee33cbf2e49957bea0142de0cd4ddf2dfa72
Jhúlio Castro <jhuliocastro@hotmail.com>
2022-04-02 20:50:09 -0300

    PRODUTOS ORDENADOS POR ONDEM ALFABETICA

--- core/views/produtosRelacao.php
+++ core/views/produtosRelacao.php
@@ -85,6 +85,7 @@ $this->data["empresa"] = EMPRESA;
 
         tabela = $('#tabela').DataTable({
             "paging": false,
+            "order": [[1, "asc"]],
             'ajax': '/produtos/tabela',
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json"
