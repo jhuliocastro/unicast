b1d0a5cd732b66a08e4da0364c801a70bf7d3e3d
Jhúlio Castro <jhuliocastro@hotmail.com>
2022-04-17 11:34:36 -0300

    CLASSE DE TABELA DO FRAMEWORK

--- core/TTable.php
+++ core/TTable.php
@@ -6,12 +6,17 @@ class TTable{
     private string $id;
     private string $column;
     private string $data;
+    private ?string $url = null;
+    private bool $paginacao = true;
+    private int $numberPaging = 10;
+    private int $numberColumnOrder = 0;
+    private string $orderTable = "asc";
 
     public function __construct(string $id)
     {
         $this->id = $id;
         $this->table = "
-            <table id='tabela' class='display compact' style='width: 100%;'>
+            <table id='$id' class='display compact' style='width: 100%;'>
         ";
 
         $this->column = "
@@ -27,8 +32,53 @@ class TTable{
 
     }
 
+    public function paging(bool $paging, int $count){
+        $this->paginacao = $paging;
+        $this->numberPaging = $count;
+    }
+
     public function addColumn(string $column){
         $column = "<td>$column</td></tr>";
         $this->column = str_replace("</tr>", $column, $this->column);
+    }
+
+    public function urlData(string $url){
+        $this->url = $url;
+    }
+
+    /**
+     * @param int $column (Number of column)
+     * @param string $order (asc or desc)
+     */
+
+    public function order(int $column, string $order){
+        $this->numberColumnOrder = $column;
+        $this->orderTable = $order;
+    }
+
+    public function close(){
+        if($this->paginacao == 1){
+           $paginacao = "true";
+        }else{
+            $paginacao = "false";
+        }
+
+        $this->table .= $this->column;
+        $this->table .= "</table>";
+        $this->table .= "
+        <script>
+            var tabela = $('#$this->id').DataTable({
+                'paging': $paginacao,
+                'order': [[$this->numberColumnOrder, '$this->orderTable']],
+                'iDisplayLength': $this->numberPaging,
+                'ajax': '$this->url',
+                'language': {
+                    'url': '//cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+                }
+            });
+        </script>
+        ";
+
+        return $this->table;
     }
 }
