c19b4cc62a4ca32c8ec75dc9a6697b3e35e220b8
Jhulio Castro <jhuliocastro@hotmail.com>
2022-06-08 12:45:06 -0300

    ATUALIZACAO DA VERIFICACAO DO BANCO DE DADOS SCRIPT

--- scripts/verificaBanco.php
+++ scripts/verificaBanco.php
@@ -21,6 +21,169 @@ if ($error) {
  * FETCH DATA
  */
 
+$tabela = "nfe_produtos";
+
+print("VERIFICANDO TABELA '$tabela'\n");
+
+$vendas = $connect->query("SHOW TABLES LIKE '$tabela'")->rowCount() > 0;
+
+if($vendas <= 0){
+    print("CRIANDO TABELA '$tabela'\n");
+    $connect->query("CREATE TABLE $tabela (`id` INT NOT NULL AUTO_INCREMENT , PRIMARY KEY (`id`))");
+}
+
+print("TABELA '$tabela' OK");
+
+$vendas = $connect->query("SHOW COLUMNS FROM $tabela");
+//var_dump($users->fetchAll());
+$vendas = $vendas->fetchAll();
+
+print("-------------------------\n");
+
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "id";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`$coluna`);");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "chave";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` VARCHAR(255) NOT NULL;");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "produto";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` VARCHAR(255) NOT NULL;");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "codigoBarras";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` VARCHAR(255) NOT NULL;");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "valor";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` float NOT NULL;");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "created_at";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` TIMESTAMP NULL");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+//INICIO COLUNA -------------------------------------------------------------------------------------------
+$coluna = "updated_at";
+
+print("PROCURANDO COLUNA '$coluna'\n");
+$retorno = false;
+foreach($vendas as $venda){
+    if($venda->Field == $coluna){
+        $retorno = true;
+    }
+}
+if($retorno == false){
+    print("CRIANDO COLUNA '$coluna'\n");
+    $id = $connect->query("ALTER TABLE `$tabela` ADD `$coluna` TIMESTAMP NULL");
+}
+
+print("COLUNA '$coluna' OK\n");
+
+print("-------------------------\n");
+
+//FIM COLUNA ----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 $tabela = "nfe";
 
 print("VERIFICANDO TABELA '$tabela'\n");
