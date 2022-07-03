0367330197a44be3a187543f016195b9620db45b
Jhulio Castro <jhuliocastro@hotmail.com>
2022-07-03 00:13:37 -0300

    ATUALIZACAO DA DASHBOARD

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -3,23 +3,62 @@ $this->layout("_theme", $this->data);
 $this->data["empresa"] = EMPRESA;
 ?>
 
-<div class="pcoded-content">
-    <div class="card">
-        <div class="card-header">
-            Dashboard :: <?= $this->data["empresa"] ?>
-        </div>
-        <div class="card-body p-2">
-            <div class="card-block">
-                <div class="container-fluid">
-                    <div class="col-md-3">
-                        <div class="icon-box border bd-default">
-                            <div class="icon bg-cyan fg-white"><span class="mif-widgets"></span></div>
-                            <div class="content p-4">
-                                <div class="text-upper">Produtos Cadastrados</div>
-                                <div class="text-upper text-bold text-lead"><?= $this->data["totalProdutos"] ?></div>
-                            </div>
-                        </div>
-                    </div>
+<style>
+    .order-card {
+    color: #fff;
+}
+
+.bg-c-blue {
+    background: linear-gradient(45deg,#4099ff,#73b4ff);
+}
+
+.bg-c-green {
+    background: linear-gradient(45deg,#2ed8b6,#59e0c5);
+}
+
+.bg-c-yellow {
+    background: linear-gradient(45deg,#FFB64D,#ffcb80);
+}
+
+.bg-c-pink {
+    background: linear-gradient(45deg,#FF5370,#ff869a);
+}
+
+
+.card {
+    border-radius: 5px;
+    -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    border: none;
+    margin-bottom: 30px;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+.card .card-block {
+    padding: 25px;
+}
+
+.order-card i {
+    font-size: 26px;
+}
+
+.f-left {
+    float: left;
+}
+
+.f-right {
+    float: right;
+}
+</style>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 col-xl-3">
+            <div class="card bg-c-blue order-card">
+                <div class="card-block">
+                    <h6 class="m-b-20">Produtos Cadastrados</h6>
+                    <h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span><?= $this->data["totalProdutos"]; ?></span></h2>
                 </div>
             </div>
         </div>
